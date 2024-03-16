f342d4c5838a1c7f187b79a485b581f339909aab
levo-777 <levan.mebonia@protonmail.com>
2024-03-16 19:35:01 +0100

    update

---
+++ part2/countrydata/src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+
+
+
+function App() {
+  const [country, setCountry] = useState([])
+
+  useEffect
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
